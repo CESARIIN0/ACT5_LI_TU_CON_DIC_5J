21b33d85b4fa5e3d79631fbade09866e455b00c2
CESARIIN0 <a.22308051281294@cbtis128.edu.mx>
2024-09-11 07:49:47 -0600

    Add files via upload

---
+++ mis_conjuntos.py
@@ -0,0 +1,8 @@
+print("ejemlos conjuntos")
+tipodeportes={"soccer" , "fut" , "beisbol"}
+print (tipodeportes)
+print("lista de elementos de conjunto deportes loop for")
+print(tipodeportes)
+for cesarin in tipodeportes:
+    print(cesarin)
+    
